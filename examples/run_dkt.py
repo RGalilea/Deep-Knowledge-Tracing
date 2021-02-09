8775a38c3534bb65cedb0f5adb550ea96c179455
RGalilea <75454302+RGalilea@users.noreply.github.com>
2021-02-09 09:31:29 -0300

    Add files via upload

--- examples/run_dkt.py
+++ examples/run_dkt.py
@@ -3,13 +3,44 @@ import tensorflow as tf
 from deepkt import deepkt, data_util, metrics
 import time
 
+
+def custom_loss(l_r,l_w1,l_w2):
+    def true_loss(y_true, y_pred):
+        #print(holder)
+        y_true2, y_pred2 = data_util.get_target(y_true[:, :-1, :], y_pred[:, 1:, :])#is this shit right??
+        y_true2 = tf.concat([tf.zeros([1,1,1],tf.float32),y_true2],axis=-2)
+        y_pred2 = tf.concat([tf.zeros([1,1, 1], tf.float32),y_pred2], axis=-2)
+
+        n_batch,n_inter,n_outputs = y_true.shape
+
+        waviness_norm_1 = tf.reduce_sum(tf.abs(y_pred[:, 1:, :] - y_pred[:, :-1, :]))/(n_inter*n_outputs )
+
+        waviness_norm_2 = tf.reduce_sum(tf.square(y_pred[:, 1:, :] - y_pred[:, :-1, :]))/(n_inter*n_outputs )
+
+
+        y_true, y_pred = data_util.get_target(y_true, y_pred)
+
+        def get_loss_value(a,b,c,d,e,f):
+            return tf.keras.losses.binary_crossentropy(a,b) + l_r*tf.keras.losses.binary_crossentropy(c, d) +l_w1*e +l_w2*f#(y_true, y_pred)
+        return get_loss_value(y_true, y_pred,y_true2, y_pred2,waviness_norm_1,waviness_norm_2)
+
+    return true_loss
+
+
 def run(args):
     start = time.time()
     print("[----- LOADING DATASET  ------]")
-    dataset, length, nb_features,_,_  = data_util.load_dataset_w_difficulty(fn=args.f, fn2=args.classes,
-                                                                              batch_size=args.batch_size,
-                                                                              level=args.l,
-                                                                              shuffle=True)
+    #dataset, length, nb_features,_,_  = data_util.load_dataset_w_difficulty(fn=args.f, fn2=args.classes,
+    #                                                                          batch_size=args.batch_size,
+    #                                                                          level=args.l,
+    #                                                                          shuffle=True)
+
+
+
+    dataset, length, nb_features,_,_  = data_util.load_dataset_w_difficulty_filter(fn=args.f, fn2=args.classes, asignatura="Matemáticas",
+                                                                                   batch_size=args.batch_size, shuffle=True, level=args.l)
+
+
     print("[----- DIVIDING DATASET  ------]")
     train_set, test_set, val_set = data_util.split_dataset(dataset=dataset,
                                                            total_size=length,
@@ -21,7 +52,12 @@ def run(args):
                             hidden_units=args.hidden_units,
                             extra_inputs=5,#for the 5 different dificulties
                             dropout_rate=args.dropout_rate)
-    model.compile(optimizer='adam', metrics=[metrics.BinaryAccuracy(),
+
+
+
+    model.compile(optimizer='adam',
+                  loss_func= custom_loss(0.1,0.03,0.3),# i need to fix this value asignation
+                  metrics=[metrics.BinaryAccuracy(),
                                              metrics.AUC(),
                                              metrics.Precision(),
                                              metrics.Recall()])
@@ -64,6 +100,11 @@ def parse_args():
                         type=str,
                         default='nivel 1 prueba de transición',
                         help="nivel 1, 2 ó 3 prueba de transición ")
+
+    parser.add_argument("-Asignatura",
+                        type=str,
+                        default='Todas',
+                        help="Matemáticas, Ciencias, Lenguaje, Historia o Todas")
 
     # True will load weights and train them with the provided data, mind that the network arquitectures match
     parser.add_argument("-re_train",
