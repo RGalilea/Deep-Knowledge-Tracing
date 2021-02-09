2b4c95ecc217d2db0ff7d85b6bd276d486e45a72
RGalilea <75454302+RGalilea@users.noreply.github.com>
2021-02-09 09:49:18 -0300

    Update run_dkt.py

--- examples/run_dkt.py
+++ examples/run_dkt.py
@@ -3,30 +3,6 @@ import tensorflow as tf
 from deepkt import deepkt, data_util, metrics
 import time
 
-
-def custom_loss(l_r,l_w1,l_w2):
-    def true_loss(y_true, y_pred):
-        #print(holder)
-        y_true2, y_pred2 = data_util.get_target(y_true[:, :-1, :], y_pred[:, 1:, :])#is this shit right??
-        y_true2 = tf.concat([tf.zeros([1,1,1],tf.float32),y_true2],axis=-2)
-        y_pred2 = tf.concat([tf.zeros([1,1, 1], tf.float32),y_pred2], axis=-2)
-
-        n_batch,n_inter,n_outputs = y_true.shape
-
-        waviness_norm_1 = tf.reduce_sum(tf.abs(y_pred[:, 1:, :] - y_pred[:, :-1, :]))/(n_inter*n_outputs )
-
-        waviness_norm_2 = tf.reduce_sum(tf.square(y_pred[:, 1:, :] - y_pred[:, :-1, :]))/(n_inter*n_outputs )
-
-
-        y_true, y_pred = data_util.get_target(y_true, y_pred)
-
-        def get_loss_value(a,b,c,d,e,f):
-            return tf.keras.losses.binary_crossentropy(a,b) + l_r*tf.keras.losses.binary_crossentropy(c, d) +l_w1*e +l_w2*f#(y_true, y_pred)
-        return get_loss_value(y_true, y_pred,y_true2, y_pred2,waviness_norm_1,waviness_norm_2)
-
-    return true_loss
-
-
 def run(args):
     start = time.time()
     print("[----- LOADING DATASET  ------]")
@@ -56,7 +32,6 @@ def run(args):
 
 
     model.compile(optimizer='adam',
-                  loss_func= custom_loss(0.1,0.03,0.3),# i need to fix this value asignation
                   metrics=[metrics.BinaryAccuracy(),
                                              metrics.AUC(),
                                              metrics.Precision(),
